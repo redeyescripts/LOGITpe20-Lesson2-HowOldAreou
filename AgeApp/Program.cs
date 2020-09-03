ce6de6e0f4c54ec5cfb3b3c7a07e60b38696f3f7
Opilane TTHK <opilane@tthk.ee>
2020-09-03 12:18:15 +0300

    first commit

---
+++ AgeApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HowOldAreou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, What is your Name?");
+            String Name;
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + Name);
+
+
+
+
+
+
+
+            Console.WriteLine("What Year are you born in?");
+
+           
+           
+            string UserInput;
+            UserInput = Console.ReadLine();
+            int Year;
+            Year = Int32.Parse(UserInput);
+            int Age = 2020 - Year;
+            Console.WriteLine("You are "  + Age + " old.");
+
+
+            Console.WriteLine("hello!," + Name  +Year);
+
+
+        }
+    }
+}
